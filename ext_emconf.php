e389f866037282a39f183110e98f37650227ea6c
hoelbing <carsten@hoelbing.net>
2023-06-14 09:18:27 +0200

    fix title and update version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
  * Extension Manager/Repository config file for ext "theme_eahjena_course".
  */
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'theme eahjena course',
+    'title' => 'EAH Jena - Themes for the course sites',
     'description' => '',
     'category' => 'templates',
     'constraints' => [
@@ -19,12 +19,12 @@ $EM_CONF[$_EXTKEY] = [
             'ErnstAbbeHochschuleJena\\ThemeEahjenaCourse\\' => 'Classes',
         ],
     ],
-    'state' => 'stable',
+    'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Carsten Hoeelbing',
     'author_email' => 'carsten.hoelbing@eah-jena.de',
     'author_company' => 'Ernst-Abbe-Hochschule Jena',
-    'version' => '1.0.0',
+    'version' => '0.0.1',
 ];
