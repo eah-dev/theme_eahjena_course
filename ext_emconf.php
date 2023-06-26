732904d4dd6d712d8c3942039e2243869bf31f43
hoelbing <carsten@hoelbing.net>
2023-06-26 13:32:26 +0200

    update bootstrap_package

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,19 @@
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'EAH Jena - Themes for the course sites',
-    'description' => '',
+    'description' => 'Sitepackage for the EAH Jena (course)',
     'category' => 'templates',
+    'version' => '0.0.1',
+    'state' => 'beta',
+    'author' => 'Carsten Hoelbing',
+    'author_email' => 'carsten.hoelbing@eah-jena.de',
+    'author_company' => 'Ernst-Abbe-Hochschule Jena',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
-            'bootstrap_package' => '12.0.0-13.9.99',
-        ],
-        'conflicts' => [
+            'bootstrap_package' => '13.0.0-14.9.99'
         ],
     ],
     'autoload' => [
@@ -19,12 +25,4 @@ $EM_CONF[$_EXTKEY] = [
             'ErnstAbbeHochschuleJena\\ThemeEahjenaCourse\\' => 'Classes',
         ],
     ],
-    'state' => 'beta',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author' => 'Carsten Hoeelbing',
-    'author_email' => 'carsten.hoelbing@eah-jena.de',
-    'author_company' => 'Ernst-Abbe-Hochschule Jena',
-    'version' => '0.0.1',
 ];
