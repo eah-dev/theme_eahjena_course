7e823c8b03a6dba89503e1282a40bfeaa0278245
hoelbing <carsten@hoelbing.net>
2023-07-19 18:08:58 +0200

    clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,2 @@
 <?php
 defined('TYPO3') or die('Access denied.');
-/***************
- * Add default RTE configuration
- */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['theme_eahjena_course'] = 'EXT:theme_eahjena_course/Configuration/RTE/Default.yaml';
-
-/***************
- * PageTS
- */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme_eahjena_course/Configuration/TsConfig/Page/All.tsconfig">');
